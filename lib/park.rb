eb57c6f65847af4406dbbd9464a34c540b1c3d16
Zach T <ztrokey@gmail.com>
2021-04-28 10:41:14 -0600

    shows commented work on trails_by_level

--- lib/park.rb
+++ lib/park.rb
@@ -21,7 +21,32 @@ class Park
   def hikeable_miles
     @trails.sum do |trail|
       trail.length
-    end 
+    end
   end
 
+  def trails_by_level
+    by_level = {}
+    @trails.each do |trail|
+      if !by_level[trail.level]
+        by_level[trail.level] = [trail.name]
+      else
+        by_level[trail.level] += [trail.name]
+      end 
+    end
+    by_level
+
+    #doesn't return all trail names below
+    # by_level = {}
+    # @trails.each do |trail|
+    #   by_level[trail.level] = [trail.name]
+    # end
+    # by_level
+    # require 'pry'; binding.pry
+  end
 end
+#use if statement like this
+#if !snacks[item]
+# snacks[item] = quantity
+# else
+#   snacks[item] += quantity
+# end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -57,4 +57,44 @@ RSpec.describe Park do
       expect(park1.hikeable_miles).to eq(6.9)
     end
   end
+
+  describe '#trails_by_level' do
+    it 'shows trails by level' do
+      park1 = Park.new('Capitol Reef')
+      park2 = Park.new('Bryce Canyon')
+
+      trail1 = Trail.new({name: 'Grand Wash',
+                         length: '2.2 miles',
+                         level: :easy})
+      trail2 = Trail.new({name: 'Cohab Canyon',
+                          length: '1.7 miles',
+                          level: :moderate})
+      trail3 = Trail.new({name: 'Chimney Rock Loop',
+                          length: '3.6 miles',
+                          level: :strenuous})
+      trail4 = Trail.new({name: "Queen's/Navajo Loop",
+                          length: '2.9 miles',
+                          level: :moderate})
+      trail5 = Trail.new({name: 'Rim Trail',
+                          length: '11 miles',
+                          level: :easy})
+      trail6 = Trail.new({name: 'Tower Bridge',
+                          length: '3 miles',
+                          level: :moderate})
+
+      park1.add_trail(trail1)
+      park1.add_trail(trail2)
+      park1.add_trail(trail3)
+      park2.add_trail(trail4)
+      park2.add_trail(trail5)
+      park2.add_trail(trail6)
+
+      # expected1 = {:easy=>["Grand Wash"],
+      #             :moderate=>["Cohab Canyon"],
+      #             :strenuous=>["Chimney Rock Loop"]}
+      expected2 = 4
+      # expect(park1.trails_by_level).to eq(expected1)
+      expect(park2.trails_by_level).to eq(expected2)
+    end
+  end
 end
